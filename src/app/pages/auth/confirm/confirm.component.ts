1495177b0977e1587c49f68c312c9e0991bc08ea
Zied Kanoun <zied.kanoun6@gmail.com>
2022-01-27 11:11:33 +0100

    added confirm email component

---
+++ src/app/pages/auth/confirm/confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.scss'],
+})
+export class ConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
