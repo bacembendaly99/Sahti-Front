d9bf3582de2f2890b085df3a34da05b90b7aa998
Zied Kanoun <zied.kanoun6@gmail.com>
2022-02-16 01:22:20 +0100

    tryed to fix it hope it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils';
-import { SeoService } from './@core/utils';
+import {Component, OnInit} from '@angular/core';
+import {AnalyticsService} from './@core/utils';
+import {SeoService} from './@core/utils';
 
 @Component({
   selector: 'ngx-app',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
 
